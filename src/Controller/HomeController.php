dd1f52fc00caef970bef6ec40c4bcb49371dc1b7
Telath <clement.pouillart@gmail.com>
2023-02-20 00:06:48 +0100

    Ajout de l'API des citations d'animes sur la page d'accueil + style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,13 +15,14 @@ class HomeController extends AbstractController
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, "https://animechan.vercel.app/api/random");
         curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $data = curl_exec($curl);
         $obj = json_decode($data, true);
         curl_close($curl);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'obj' => $obj,
+            'quote' => $obj,
         ]);
     }
 }
